3b7756d97873a762be06074906d659dd420eb454
tragicallyludicrous <95321639+tragicallyludicrous@users.noreply.github.com>
2026-06-16 15:07:42 -0400

    workingish

--- image-sandpile.js
+++ image-sandpile.js
@@ -2,8 +2,9 @@ let img;
 let dsImg;
 let outImg;
 let deltaMask;
-const DS_FACTOR = 10;
-const BRIGHTEN_VALUE = 100;
+const DS_FACTOR = 4; // 10;
+const BRIGHTEN_VALUE = 1024 * 1024; // 1024; // 512; // 256; // 128;
+const MAX_GRAINS = 255;
 let newWidth;
 let newHeight;
 
@@ -14,36 +15,48 @@ function preload() {
 }
 function setup() {
   createCanvas(displayHeight, displayHeight);
-  background(100);
+  background("white");
   dsImg = downscale(img, DS_FACTOR);
+  outImg = downscale(img, DS_FACTOR);
+  makeDeltaMask(outImg);
 }
+
 function draw() {
-  image(dsImg, 0, 0, displayHeight, displayHeight);
   noSmooth();
-}
 
-function mouseClicked() {
-  let x = (mouseX / displayHeight) * dsImg.width;
-  let y = (mouseY / displayHeight) * dsImg.height;
-  console.log(`MouseX: ${mouseX}, MouseY: ${mouseY}`);
-  console.log(Math.floor(x), Math.floor(y));
-  brighten(x, y);
-}
-
-function brighten(x, y) {
-  px = dsImg.get(x, y);
-  // returns [R, G, B, A]
-
-  for (let i = 0; i < 2; i++) {
-    px[i] = (px[i] + BRIGHTEN_VALUE) % 255;
+  if (mouseIsPressed) {
+    let x = (mouseX / displayHeight) * dsImg.width;
+    let y = (mouseY / displayHeight) * dsImg.height;
+    increaseDeltaMask(Math.floor(x), Math.floor(y), BRIGHTEN_VALUE);
   }
-  // px = [255, 255, 255, 255];
 
-  dsImg.set(x, y, px);
-  dsImg.updatePixels();
+  stepAbelianSandpile();
+  brightenImage();
+  image(dsImg, 0, 0, displayHeight, displayHeight);
+  image(outImg, 0, 0, displayHeight, displayHeight);
 }
 
-function getDeltaMask(img) {
+// function mouseClicked() {
+//   let x = (mouseX / displayHeight) * dsImg.width;
+//   let y = (mouseY / displayHeight) * dsImg.height;
+//   // console.log(`MouseX: ${mouseX}, MouseY: ${mouseY}`);
+//   // console.log(Math.floor(x), Math.floor(y));
+//   increaseDeltaMask(Math.floor(x),Math.floor(y), BRIGHTEN_VALUE);
+// }
+
+function getDeltaMask(x, y) {
+  return deltaMask[y][x];
+}
+
+function setDeltaMask(x, y, amt) {
+  deltaMask[y][x] = amt;
+}
+
+function increaseDeltaMask(x, y, amt) {
+  deltaMask[y][x] += amt;
+}
+
+function makeDeltaMask(img) {
   const rows = img.height;
   const cols = img.width;
   deltaMask = Array.from({ length: rows }, () => Array(cols).fill(0));
@@ -55,19 +68,18 @@ function brightenImage() {
 
   for (let y = 0; y < dsImg.height; y++) {
     for (let x = 0; x < dsImg.width; x++) {
-      let sourceIndex = x + y * dsImg.width * 4;
+      let sourceIndex = (x + y * dsImg.width) * 4;
 
       outImg.pixels[sourceIndex + 0] =
-        (dsImg.pixels[sourceIndex + 0] + BRIGHTEN_VALUE * deltaMask[y][x]) %
-        255; // R
+        (dsImg.pixels[sourceIndex + 0] + deltaMask[y][x]) % 255; // R
       outImg.pixels[sourceIndex + 1] =
-        (dsImg.pixels[sourceIndex + 1] + BRIGHTEN_VALUE * deltaMask[y][x]) %
-        255; // G
+        (dsImg.pixels[sourceIndex + 1] + deltaMask[y][x]) % 255; // G
       outImg.pixels[sourceIndex + 2] =
-        (dsImg.pixels[sourceIndex + 2] + BRIGHTEN_VALUE * deltaMask[y][x]) %
-        255; // B
+        (dsImg.pixels[sourceIndex + 2] + deltaMask[y][x]) % 255; // B
+      // setting 50% opacity
+      outImg.pixels[sourceIndex + 3] = 128;
     }
-    destImg.updatePixels();
+    outImg.updatePixels();
   }
 }
 
@@ -98,4 +110,68 @@ function downscale(sourceImg, n) {
   }
   destImg.updatePixels();
   return destImg;
+}
+
+function stepAbelianSandpile() {
+  for (let y = 0; y < dsImg.height; y++) {
+    for (let x = 0; x < dsImg.width; x++) {
+      stepAbelianSandpileSlot(x, y);
+    }
+  }
+}
+
+function stepAbelianSandpileSlot(x, y) {
+  let lastGrains = getDeltaMask(x, y);
+  if (lastGrains < MAX_GRAINS) {
+  } else {
+    setDeltaMask(x, y, 0);
+    var num_valid_directions = 0;
+    num_valid_directions += 0 < x ? 1 : 0;
+    num_valid_directions += x < dsImg.width ? 1 : 0;
+    num_valid_directions += 0 < y ? 1 : 0;
+    num_valid_directions += y < dsImg.height ? 1 : 0;
+    let to_distribute = Math.floor(lastGrains / num_valid_directions);
+    let leftover_grains = lastGrains % num_valid_directions;
+    var north_grains = to_distribute;
+    var south_grains = to_distribute;
+    var east_grains = to_distribute;
+    var west_grains = to_distribute;
+    var hasDistributedLeftover = false;
+    // north
+    if (y < dsImg.height) {
+      if (hasDistributedLeftover) {
+        increaseDeltaMask(x, y + 1, north_grains);
+      } else {
+        hasDistributedLeftover = true;
+        increaseDeltaMask(x, y + 1, north_grains + leftover_grains);
+      }
+    }
+    // south
+    if (0 < y) {
+      if (hasDistributedLeftover) {
+        increaseDeltaMask(x, y - 1, south_grains);
+      } else {
+        hasDistributedLeftover = true;
+        increaseDeltaMask(x, y - 1, south_grains + leftover_grains);
+      }
+    }
+    // east
+    if (x < dsImg.width) {
+      if (hasDistributedLeftover) {
+        increaseDeltaMask(x + 1, y, east_grains);
+      } else {
+        hasDistributedLeftover = true;
+        increaseDeltaMask(x + 1, y, east_grains + leftover_grains);
+      }
+    }
+    // west
+    if (0 < x) {
+      if (hasDistributedLeftover) {
+        increaseDeltaMask(x - 1, y, west_grains);
+      } else {
+        hasDistributedLeftover = true;
+        increaseDeltaMask(x - 1, y, west_grains + leftover_grains);
+      }
+    }
+  }
 }
